10ddb1ffecc5c42584d8e137a1ba5a70626799ef
Trent Brew <tbrew1023@gmail.com>
2024-01-20 12:15:09 -0600

    fix json parser

--- scripts.js
+++ scripts.js
@@ -1,1 +1,29 @@
-var intputBox = document.getElementById("inputBox");
+var input = document.getElementById("inputBox");
+var button = document.getElementById("runButton");
+
+function fetchSong() {
+  console.log("finding song...");
+
+  const query = input.value;
+  console.log("request: ", query || "no query...");
+
+  fetch(`https://dj-llm.vercel.app/v1/dj`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ prompt: query }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json(); // Parse the JSON of the response
+    })
+    .then((data) => {
+      console.log("response: ", data); // Handle the data from the response
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
+}
